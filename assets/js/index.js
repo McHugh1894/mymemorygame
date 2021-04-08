18251f75c79e7a84a3b873f0bca0317c912ef5d7
McHugh1894 <mchugh1894@gmail.com>
2021-04-08 10:52:59 +0000

    Added player name and timer function to index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -91,3 +91,32 @@ const currentDate = new Date();
 
 // Show current year in the footer
 footerYear.innerHTML = currentDate.getFullYear();
+
+// Player name
+function playerName() {
+    var name = document.getElementById('name-input').value;
+
+    if (name != null) {
+        document.getElementById('player').innerHTML = `Welcome ${name}, enjoy.`;
+    }
+}
+// Timer
+var minutesLabel = document.getElementById("minutes");
+var secondsLabel = document.getElementById("seconds");
+var totalSeconds = 0;
+setInterval(setTime, 1000);
+
+function setTime() {
+    ++totalSeconds;
+    secondsLabel.innerHTML = pad(totalSeconds % 60);
+    minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+}
+
+function pad(val) {
+    var valString = val + "";
+    if (valString.length < 2) {
+        return "0" + valString;
+    } else {
+        return valString;
+    }
+}
