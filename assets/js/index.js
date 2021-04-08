bdde17c74f18c3cd4a10e029d322e692fd1e23fc
McHugh1894 <mchugh1894@gmail.com>
2021-04-08 10:49:32 +0000

    Added card config and function to index.js

---
+++ assets/js/index.js
@@ -0,0 +1,78 @@
+// Card CONFIG
+const CARD_CONFIG = [{
+        name: 'Alfa-Romeo',
+        imagePathName: 'Alfa-Romeo-logo.jpg'
+    },
+    {
+        name: 'Aston-Martin',
+        imagePathName: 'Aston-Martin-logo.jpg'
+    },
+    {
+        name: 'Audi',
+        imagePathName: 'Audi-logo.jpg'
+    },
+    {
+        name: 'BMW',
+        imagePathName: 'bmw-logo.png'
+    },
+    {
+        name: 'Chevrolet',
+        imagePathName: 'Chevrolet-logo.jpg'
+    },
+    {
+        name: 'Ferrari',
+        imagePathName: 'ferrari-logo.png'
+    },
+    {
+        name: 'Fiat',
+        imagePathName: 'Fiat-logo.jpg'
+    },
+    {
+        name: 'Ford',
+        imagePathName: 'ford-logo.png'
+    },
+    {
+        name: 'Honda',
+        imagePathName: 'honda-logo.png'
+    },
+    {
+        name: 'Lamborghini',
+        imagePathName: 'lamborghini-logo.png'
+    },
+    {
+        name: 'Mercedes-Benz',
+        imagePathName: 'Mercedes-Benz-logo.jpg'
+    },
+    {
+        name: 'Porsche',
+        imagePathName: 'porsche-logo.png'
+    },
+    {
+        name: 'Tesla',
+        imagePathName: 'tesla-logo.png'
+    },
+    {
+        name: 'Toyota',
+        imagePathName: 'toyota-logo.png'
+    },
+    {
+        name: 'Volkswagen',
+        imagePathName: 'Volkswagen-logo.jpg'
+    },
+
+]
+var cardsHtml = ""
+
+function generateCards() {
+    var cards = [...CARD_CONFIG, ...CARD_CONFIG].forEach(eachCard => {
+        cardsHtml += `
+                         <div class="card" data-carddata="${eachCard.name}">
+                <img class="front-face" src="assets/images/${eachCard.imagePathName}" alt="card ${eachCard.name}">
+                <img class="back-face" src="assets/images/card_back.png" alt="card backside">
+            </div>
+           `
+    });
+    return cards;
+}
+var temp = generateCards()
+document.getElementById('game-board').innerHTML = cardsHtml
