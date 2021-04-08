fa24d34486648936d5424bd1031381991c98c5da
McHugh1894 <mchugh1894@gmail.com>
2021-04-08 10:55:21 +0000

    Added memory card game logic to index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -120,3 +120,42 @@ function pad(val) {
         return valString;
     }
 }
+
+// Memory card game logic
+let flippedCard = false;
+let firstCard, secondCard;
+let gameLock = true;
+let pairs = 15;
+let clicks = 0;
+
+card.forEach(cards => cards.addEventListener('click', flipOver));
+shuffle();
+
+function flipOver() {
+    if (gameLock) return;
+    if (this === firstCard) return;
+
+    this.classList.add('flip');
+
+    if (!flippedCard) {
+        // The first click, card
+        flippedCard = true;
+        firstCard = this;
+
+    } else {
+        // The second click, card
+        flippedCard = false;
+        secondCard = this;
+
+        // Checking cards match? 
+
+        if (firstCard.dataset.carddata === secondCard.dataset.carddata) {
+            // It's a match
+            match()
+
+        } else {
+            // Not a match
+            flipBack()
+        }
+    }
+}
